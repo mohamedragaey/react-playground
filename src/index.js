dbaba924eccb7df8957a6adb9116f8183a745ed8
Mohamed <mohamedragaey33@gmail.com>
2018-05-21 21:33:13 +0200

    removing unwanted imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import ErrorBoundary from './components/ErrorBoundary'
-// import Comment from './components/Comment'
 import App from './components/App'
 import './styles/style.scss'
 
